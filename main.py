c934e7be28b44d12ad2341eb673418e021e349c2
Medet <56687178+acostyle@users.noreply.github.com>
2020-12-08 14:45:54 +0600

    Add multiple download books from tululu.org

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import requests
 
+from pathlib import Path
+
 
 def download_book():
-    url = 'https://tululu.org/txt.php?id=32168'
+    for book_id in range(0, 11):
+        url = f'https://tululu.org/txt.php?id={book_id}'
 
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
+        response = requests.get(url, allow_redirects=False, verify=False)
+        response.raise_for_status()
 
-    filename = 'dvmn.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+        filename = f'books/{book_id}.txt'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
 
 
 def main():
+    Path("books").mkdir(parents=True, exist_ok=True)
     download_book()
 
 
