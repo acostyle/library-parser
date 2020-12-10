06d55656d05a833fea5b18b12ac68623e744459b
Medet <56687178+acostyle@users.noreply.github.com>
2020-12-10 18:15:46 +0600

    download books with titles

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
 import requests
 
+from bs4 import BeautifulSoup
 from pathlib import Path
+from pathvalidate import sanitize_filename
 
 
-def download_book():
-    for book_id in range(0, 11):
-        url = f'https://tululu.org/txt.php?id={book_id}'
+DOWNLOAD_URL = 'https://tululu.org/txt.php?id='
+INFO_URL = 'https://tululu.org/b'
 
-        response = requests.get(url, allow_redirects=False, verify=False)
-        response.raise_for_status()
 
-        filename = f'books/{book_id}.txt'
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+def get_response_with_book_data(book_id):
+    response = requests.get(f'{INFO_URL}{book_id}/',
+                            allow_redirects=False, verify=False)
+    response.raise_for_status()
+    return response
+
+
+def download_txt(book_id, filename, folder='books/'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+
+    url = f'{DOWNLOAD_URL}{book_id}'
+
+    response = requests.get(url, allow_redirects=False, verify=False)
+    response.raise_for_status()
+
+    path_to_save = Path(folder).joinpath(f'{filename}.txt')
+
+    with open(path_to_save, 'wb') as file:
+        file.write(response.content)
+
+    return str(path_to_save)
+
+
+def parse_title_and_author(book_data):
+    soup = BeautifulSoup(book_data.text, 'lxml')
+
+    title_and_author = soup.find('h1').text.split('\xa0 :: \xa0')
+
+    title = title_and_author[0].strip()
+    author = title_and_author[1].strip()
+
+    return sanitize_filename(title), sanitize_filename(author)
 
 
 def main():
-    Path("books").mkdir(parents=True, exist_ok=True)
-    download_book()
+    for book_id in range(1, 11):
+        book_data = get_response_with_book_data(book_id)
+
+        title, author = parse_title_and_author(book_data)
+
+        book_name = f'{book_id}. {title}'
+        book = download_txt(book_id, book_name)
 
 
 if __name__ == '__main__':
