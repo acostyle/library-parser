2fc6ab1c6fbfb3432bbaec0aad7444a3b63b68cf
Medet <56687178+acostyle@users.noreply.github.com>
2020-12-26 20:00:32 +0600

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,31 +1,40 @@
 import json
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from livereload import Server, shell
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-with open("books.json", "r", encoding='utf-8') as my_file:
-    all_books = my_file.read()
+def get_books_json():
+    with open("books.json", "r") as json_file:
+        books_json = json_file.read()
+
+        return json.loads(books_json)
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-template = env.get_template('template.html')
+    template = env.get_template('template.html')
 
-with open("books.json", "r") as json_file:
-    books_json = json_file.read()
+    books = get_books_json()
 
-books = json.loads(books_json)
+    rendered_page = template.render(
+        books=books
+    )
 
-rendered_page = template.render(
-    books=books
-)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='')
+
+
+if __name__ == '__main__':
+    main()
