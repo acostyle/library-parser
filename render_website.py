d1390f4c08dcab8d671efddbbbef18b485283dbf
Medet <56687178+acostyle@users.noreply.github.com>
2021-01-15 16:52:32 +0600

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,42 +1,46 @@
 import json
+import os
 
 from livereload import Server, shell
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
+from pathlib import Path
 
 
 def get_books_json():
-    with open("books.json", "r") as json_file:
+    with open("books.json", "r", encoding="utf8") as json_file:
         books_json = json_file.read()
-
-        return json.loads(books_json)
+    return json.loads(books_json)
 
 
 def on_reload():
+    all_books = get_books_json()
+    books_per_page = 20
+
+    chunked_books = list(chunked(all_books, books_per_page))
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
     template = env.get_template('template.html')
+    
+    for page, books in enumerate(chunked_books, 1):
+        rendered_page = template.render(
+            books=books
+        )
 
-    books = get_books_json()
-    books_per_page = 20
-
-    chunked_books = chunked(books, books_per_page)
-
-    rendered_page = template.render(
-        chunked_books=chunked_books
-    )
-
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+        with open(f'pages/index{page}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def main():
+    Path(os.getcwd(), 'pages').mkdir(parents=True, exist_ok=True)
+
     on_reload()
     server = Server()
-    server.watch('template.html', on_reload)
+    server.watch('*.html', on_reload)
     server.serve(root='')
 
 
